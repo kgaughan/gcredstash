58c208e4fb892f769a16abfbbe3bc4de4cbd420e
Keith Gaughan <k@stereochro.me>
2023-07-16 20:16:31 +0100

    Merge templating code back into command

--- internal/command/template.go
+++ internal/command/template.go
@@ -3,11 +3,13 @@ package command
 import (
 	"bytes"
 	"fmt"
+	"html/template"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/kgaughan/gcredstash/internal"
-	gcredstash "github.com/kgaughan/gcredstash/internal"
-	"github.com/kgaughan/gcredstash/internal/templating"
+	"github.com/mattn/go-shellwords"
 	"github.com/spf13/cobra"
 )
 
@@ -18,10 +20,10 @@ func templateImpl(cmd *cobra.Command, args []string) error {
 
 	var content string
 	if tmplFile == "-" {
-		content = gcredstash.ReadStdin()
+		content = internal.ReadStdin()
 	} else {
 		var err error
-		content, err = gcredstash.ReadFile(tmplFile)
+		content, err = internal.ReadFile(tmplFile)
 		if err != nil {
 			return fmt.Errorf("cannot read %q: %w", tmplFile, err)
 		}
@@ -32,7 +34,7 @@ func templateImpl(cmd *cobra.Command, args []string) error {
 		return err //nolint:wrapcheck
 	}
 
-	tmpl, err := templating.MakeTemplate(driver, table).Parse(content)
+	tmpl, err := makeTemplate(driver, table).Parse(content)
 	if err != nil {
 		return fmt.Errorf("cannot parse %q template: %w", tmplFile, err)
 	}
@@ -50,6 +52,54 @@ func templateImpl(cmd *cobra.Command, args []string) error {
 
 	cmd.Print(buf.String())
 	return nil
+}
+
+func makeTemplate(driver *internal.Driver, table string) *template.Template {
+	return template.New("template").Funcs(template.FuncMap{
+		"get": func(credential string, cxt ...string) (string, error) {
+			context, err := internal.ParseContext(cxt)
+			if err != nil {
+				return "", fmt.Errorf("could not parse context: %w", err)
+			}
+
+			value, err := driver.GetSecret(credential, "", table, context)
+			if err != nil {
+				return "", fmt.Errorf("could not fetch credentials: %w", err)
+			}
+
+			return value, nil
+		},
+		"env": func(key string) (string, error) {
+			return os.Getenv(key), nil
+		},
+		"sh": func(line string) (string, error) {
+			cmd, err := shellwords.Parse(line)
+			if err != nil {
+				return "", fmt.Errorf("could not parse command: %w", err)
+			}
+
+			var out []byte
+
+			switch len(cmd) {
+			case 0:
+				out = []byte{}
+			case 1:
+				//nolint:gosec
+				out, err = exec.Command(cmd[0]).Output()
+			default:
+				//nolint:gosec
+				out, err = exec.Command(cmd[0], cmd[1:]...).Output()
+			}
+
+			if err != nil {
+				return "", fmt.Errorf("could not run command: %w", err)
+			}
+
+			str := string(out)
+
+			return strings.TrimRight(str, "\n"), nil
+		},
+	})
 }
 
 func init() {
