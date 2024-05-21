57e83dd85a0ae2db67c522b7f0e97e0bdd81e605
Keith Gaughan <keith@gaughan.ie>
2024-05-21 11:50:04 +0100

    Make the linter happy

--- internal/command/make_driver.go
+++ internal/command/make_driver.go
@@ -8,7 +8,7 @@ import (
 func wrapWithDriver(fn func(*cobra.Command, []string, *internal.Driver) error) func(*cobra.Command, []string) error {
 	driver, err := internal.NewDriver()
 	if err != nil {
-		return func(cmd *cobra.Command, args []string) error {
+		return func(_ *cobra.Command, _ []string) error {
 			return err //nolint:wrapcheck
 		}
 	}
